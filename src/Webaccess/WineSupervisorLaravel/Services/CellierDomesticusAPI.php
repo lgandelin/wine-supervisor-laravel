6343d4c87cc9267548a3d15b98691c5f90431a8a
Louis Gandelin <lgandelin@web-access.fr>
2017-07-06 17:14:46 +0200

    Added create_technician method to api

--- src/Webaccess/WineSupervisorLaravel/Services/CellierDomesticusAPI.php
+++ src/Webaccess/WineSupervisorLaravel/Services/CellierDomesticusAPI.php
@@ -6,6 +6,7 @@ use DateTimeZone;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
 use Webaccess\WineSupervisorLaravel\Models\Cellar;
+use Webaccess\WineSupervisorLaravel\Models\Technician;
 use Webaccess\WineSupervisorLaravel\Models\User;
 
 class CellierDomesticusAPI
@@ -19,13 +20,16 @@ class CellierDomesticusAPI
         );
     }
 
-    private function generateWSSEToken()
+    private function generateWSSEToken($userName = null, $userPassword = null)
     {
+        if (!$userName) $userName = env('CD_API_USERNAME');
+        if (!$userPassword) $userPassword = env('CD_API_PASSWORD');
+
         $nonce = $this->generateNonce();
         $created = $this->generateCreated();
-        $passwordDigest = $this->generatePasswordDigest($nonce, $created);
+        $passwordDigest = $this->generatePasswordDigest($nonce, $created, $userPassword);
 
-        return sprintf('Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"', env('CD_API_USERNAME'), $passwordDigest, $nonce, $created);
+        return sprintf('Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"', $userName, $passwordDigest, $nonce, $created);
     }
 
     private function generateNonce()
@@ -38,9 +42,9 @@ class CellierDomesticusAPI
         return gmdate('Y-m-d\TH:i:s\Z');
     }
 
-    private function generatePasswordDigest($nonce, $created)
+    private function generatePasswordDigest($nonce, $created, $password)
     {
-        return base64_encode(sha1(base64_decode($nonce).$created.env('CD_API_SECRET'), true));
+        return base64_encode(sha1(base64_decode($nonce).$created.$password, true));
     }
 
     public function create_user(User $user)
@@ -66,13 +70,13 @@ class CellierDomesticusAPI
             ]
         ];
 
-        Log::info('API_CREATE_ACCOUNT_REQUEST', $requestData);
+        Log::info('API_CREATE_USER_REQUEST', $requestData);
 
         /*if ($response = $this->client->request('POST', '/api/users', $requestData)) {
             $result = $response->getBody()->getContents();
             $resultObject = json_decode($result);
 
-            Log::info('API_CREATE_ACCOUNT_RESPONSE', [
+            Log::info('API_CREATE_USER_RESPONSE', [
                 'success' => $resultObject->status,
                 'status_code' => $response->getStatusCode(),
                 'body' => $result
@@ -83,12 +87,60 @@ class CellierDomesticusAPI
             $user->cd_password = $resultObject->data->password;
             $user->save();
         } else {
-            Log::info('API_CREATE_ACCOUNT_RESPONSE', [
+            Log::info('API_CREATE_USER_RESPONSE', [
                 'success' => false,
             ]);
 
             //TODO : Re-launch the request
         }*/
+    }
+
+    public function create_technician(Technician $technician)
+    {
+        $requestData = [
+            'json' => [
+                'username' => 'testtechnician',
+                'email' => 'test@testemail11.fr',
+                'plainPassword' => '222bbb',
+                'type' => 'technician',
+                'lastName' => 'technician lastname',
+                'firstName' => 'technician firstname',
+                'address' => '1, rue test',
+                'zipcode' => '73000',
+                'city' => 'CHAMBERY',
+                'country' => 'FR',
+                'phone' => '0611061106',
+                'mobile' => '0611061106',
+            ],
+            'headers' => [
+                'Authorization' => 'profile="UsernameToken"',
+                'X-WSSE' => 'UsernameToken ' . $this->generateWSSEToken()
+            ]
+        ];
+
+        Log::info('API_CREATE_TECHNICIAN_REQUEST', $requestData);
+
+        if ($response = $this->client->request('POST', '/api/users', $requestData)) {
+            $result = $response->getBody()->getContents();
+            $resultObject = json_decode($result);
+
+            Log::info('API_CREATE_TECHNICIAN_RESPONSE', [
+                'success' => $resultObject->status,
+                'status_code' => $response->getStatusCode(),
+                'body' => $result
+            ]);
+
+            //Store the CD userID and password
+            $technician->cd_user_id = $resultObject->data->id;
+            $technician->cd_password = $resultObject->data->password;
+            $technician->save();
+        } else {
+            Log::info('API_CREATE_TECHNICIAN_RESPONSE', [
+                'success' => false,
+            ]);
+
+            //TODO : Re-launch the request
+        }
     }
 
     public function activate_cellar(Cellar $cellar)
@@ -137,7 +189,7 @@ class CellierDomesticusAPI
         $requestData = [
             'headers' => [
                 'Authorization' => 'profile="UsernameToken"',
-                'X-WSSE' => 'UsernameToken ' . $this->generateWSSEToken()
+                'X-WSSE' => 'UsernameToken ' . $this->generateWSSEToken($user->login, $user->cd_password)
             ]
         ];
 
