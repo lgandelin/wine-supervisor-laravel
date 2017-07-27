c809a5dcd9fe8229aae7200b1db4e03f7094d8d2
Louis Gandelin <lgandelin@web-access.fr>
2017-07-27 11:39:06 +0200

    Added an email confirmation after a technician is validated

---
+++ src/resources/views/emails/technician_account.blade.php
@@ -0,0 +1,17 @@
+Bonjour,<br><br>
+
+votre compte installateur WineSupervisor a été validé.<br><br>
+
+Vous trouverez ci-dessous votre accès afin de vous connecter au site.<br><br>
+
+<strong>Login : </strong>{{ $login }}<br><br>
+
+<strong>Mot de passe : </strong>vous seul le connaissez<br><br>
+
+Vous pouvez accéder directement au site en <a href="{{ $url }}">cliquant içi</a>.<br/><br/>
+
+Cordialement,<br><br>
+
+L'équipe WineSupervisor<br/><br/>
+
+<i>Ceci est un mail automatique, merci de ne pas y répondre.</i>
