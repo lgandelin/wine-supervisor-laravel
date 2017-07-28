838e89f062c8c333af242a9da37acc3454436f0f
Louis Gandelin <lgandelin@web-access.fr>
2017-07-28 17:43:32 +0200

    Added a condition on no index

--- src/resources/views/includes/header.blade.php
+++ src/resources/views/includes/header.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
     <meta name="viewport" content="width=1200">
-    <meta name="robots" content="noindex, nofollow">
+    @if (env('META_ROBOTS_NO_INDEX'))<meta name="robots" content="noindex, nofollow">@endif
     <title>@yield('page-title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
