524974ce0103c38e6e267d4ae2e8a7108a5738c3
Louis Gandelin <lgandelin@web-access.fr>
2017-07-31 10:38:51 +0200

    Added cookie banner

--- src/resources/views/includes/header.blade.php
+++ src/resources/views/includes/header.blade.php
@@ -10,5 +10,7 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}" />
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script type="text/javascript" id="cookiebanner" src="{{ asset('js/vendor/cookiebanner.min.js') }}" data-position="top" data-fg="#ffffff" data-bg="#333" data-link="#fff" data-moreinfo="http://www.cnil.fr/vos-obligations/sites-web-cookies-et-autres-traceurs/que-dit-la-loi/" data-message="Nous utilisons des cookies pour vous garantir une meilleure expérience sur notre site. Si vous continuez à utiliser ce dernier, nous considérerons que vous acceptez l'utilisation de cookies. Le sommelier vous conseille une bouteille de rouge pour les accompagner." data-linkmsg="En savoir plus"></script>
+
 </head>
 <body>
