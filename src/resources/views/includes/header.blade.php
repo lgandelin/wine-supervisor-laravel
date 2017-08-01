a5e0e8114e4849efa046f20e9568195ec6042c24
Louis Gandelin <lgandelin@web-access.fr>
2017-08-01 18:30:54 +0200

    Removed meta viewport

--- src/resources/views/includes/header.blade.php
+++ src/resources/views/includes/header.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=1200">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @if (env('META_ROBOTS_NO_INDEX'))<meta name="robots" content="noindex, nofollow">@endif
     <title>@yield('page-title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
